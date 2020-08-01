8031ec6f424f7b834cc97e875911576266e63e2a
user <user@DESKTOP-VLL6NJI>
2020-08-01 20:02:21 +0900

    member login

---
+++ src/test/MemberDaoInsertTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdbc.connection.ConnectionProvider;
+import member.dao.MemberDao;
+import member.model.Member;
+
+/**
+ * Servlet implementation class MemberDaoInsertTest
+ */
+@WebServlet("/MemberDaoInsertTest")
+public class MemberDaoInsertTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberDaoInsertTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		
+		try {
+			MemberDao dao = new MemberDao();
+			conn = ConnectionProvider.getConnection();
+			Member mem = new Member("hong", "123", "ȫ�浿", "010-1234-5678", "seoul", "hong@naver.com", new Date());
+			
+			dao.insert(conn, mem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
